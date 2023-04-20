94aae32c531662af40245ab742074eeba10b3723
zlhuang13 <131373559+zlhuang13@users.noreply.github.com>
2023-04-20 16:12:20 +0100

    Update histograms.py

--- histograms.py
+++ histograms.py
@@ -1,10 +1,4 @@
 import numpy as np
-from matplotlib import pyplot as plt
-
-from sklearn import datasets
-from sklearn.metrics import accuracy_score
-from sklearn import preprocessing
-
 from scipy.spatial import cKDTree
 
 class HistogramClassifier():
@@ -74,4 +68,87 @@ class HistogramClassifier():
             else:
                 self.histogram[idx]["eta"] = np.random.uniform()
             eta = self.histogram[idx]["eta"]
+        return int(eta >= 1/2)
+
+    
+class PrivHistogramClassifier():
+    def __init__(self, privacy_loss):
+        self.partition = 2
+        self.histogram = {}
+        self.labels = {}
+        self.prob = {}
+        self.kdtrees = None
+        self.cells = []
+        self.error_count = 0
+        self.distance_bound = 1/self.partition
+        self.privacy_loss = privacy_loss
+    
+    def get_histogram(self):
+        return self.histogram
+
+    def cal_index(self, x):
+        indices = np.rint(self.partition * x)
+        idx = indices.tobytes()
+        return idx, indices
+
+    def cal_index_set(self, x):
+        indices = np.rint(self.partition * x)
+        idx = [i.tobytes() for i in indices]
+        return idx, indices
+                
+    def fit(self, X_train, y_train, b):
+        """
+        Parameters
+        -------------
+        x : list, set of features
+        y : 0 or 1, set of labels in order wrt x
+        b : positive integer, cell-width parameters
+        """
+        self.partition = b
+        self.distance_bound = len(X_train[0])
+        self.dimension = len(X_train[0])
+        idx, indices = self.cal_index_set(X_train)
+        for i in range(len(X_train)):
+            try:
+                dummy = self.histogram[idx[i]]
+            except KeyError:
+                self.histogram[idx[i]] = indices[i]
+                self.labels[idx[i]] = []
+            self.labels[idx[i]].append(y_train[i])
+
+        self.cells = list(self.histogram.keys())
+        self.kdtrees = cKDTree(list(self.histogram.values()))
+
+        for cell_idx in self.cells:
+            ones = sum(self.labels[cell_idx])
+            size = len(self.labels[cell_idx])
+            eta = ones/size
+            noise = np.random.laplace(0,1/(size*self.privacy_loss))
+            eta = eta + noise
+            eta = min(1, max(eta, 0))
+            self.prob[cell_idx] = eta
+
+
+    def reset(self):
+        self.histogram.clear()
+        self.labels.clear()
+        self.prob.clear()
+        self.partition = 2
+        self.kdtrees = None
+        self.cells = []
+        self.error_count = 0
+
+    def predict(self, x):
+        idx, indices = self.cal_index(x)
+        try:
+            eta = self.prob[idx]
+        except KeyError:
+            self.error_count += 1
+            self.histogram[idx] = indices
+            if self.kdtrees != None:
+                neighbour = self.kdtrees.query(indices, k = 1, p = 1, distance_upper_bound = self.distance_bound, workers = -1)[1]
+                self.prob[idx] = self.prob[self.cells[neighbour]]
+            else:
+                self.prob[idx] = np.random.uniform()
+            eta = self.prob[idx]
         return int(eta >= 1/2)
