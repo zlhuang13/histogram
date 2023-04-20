3c4bad1147f75d0e7ed2d948dffe037cb6f348f0
zlhuang13 <131373559+zlhuang13@users.noreply.github.com>
2023-04-20 16:22:33 +0100

    Update histograms.py

--- histograms.py
+++ histograms.py
@@ -152,3 +152,12 @@ class PrivHistogramClassifier():
                 self.prob[idx] = np.random.uniform()
             eta = self.prob[idx]
         return int(eta >= 1/2)
+
+if __name__ == "__main__":
+    hist = HistogramClassifier()
+    X_train = [[0,0],[0,0.1],[0,0.2],[0.5,0.7],[0.7,0.5],[0.5,0.6]]
+    y_train = [1,1,1,0,0,0]
+    hist.fit(X_train, y_train)
+    test = [0.3,0.3]
+    label = hist.predict(test)
+    print(label)
